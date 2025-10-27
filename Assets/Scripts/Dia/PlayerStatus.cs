509918f171c4a9b56d0f32626200fbc69d79e98e
wuhancho <151606064+wuhancho@users.noreply.github.com>
2025-10-27 13:09:45 +0100

    Update PlayerStatus.cs

--- Assets/Scripts/Dia/PlayerStatus.cs
+++ Assets/Scripts/Dia/PlayerStatus.cs
@@ -7,10 +7,12 @@ public class PlayerStatus: MonoBehaviour
     [SerializeField] private float money;
     [SerializeField] private float repIglesia;
     [SerializeField] private float repPueblo;
+    [SerializeField] private float food;
 
     public float Energy { get => energy; set => energy = value; }
     public float Faith { get => faith; set => faith = value; }
     public float Money { get => money; set => money = value; }
     public float RepIglesia { get => repIglesia; set => repIglesia = value; }
     public float RepPueblo { get => repPueblo; set => repPueblo = value; }
+    public float Food { get => food; set => food = value; }
 }
