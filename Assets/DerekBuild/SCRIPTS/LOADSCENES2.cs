f2a87ceb519dd511acdd59b3c97b253c0d8b0020
DerekBaasch <98423502+DerekBaasch@users.noreply.github.com>
2026-01-11 19:17:17 +0100

    arreglo de errores parte 2

---
+++ Assets/DerekBuild/SCRIPTS/LOADSCENES2.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LOADSCENES2 : MonoBehaviour
+{
+
+
+    public void Play()
+    {
+        SceneManager.LoadScene("DÍA 2 - TARDE");
+    }
+}
---
+++ Assets/DerekBuild/SCRIPTS/LOADSCENES3.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LOADSCENES3 : MonoBehaviour
+{
+
+
+    public void Play()
+    {
+        SceneManager.LoadScene("DÍA 2 - NOCHE");
+    }
+}
