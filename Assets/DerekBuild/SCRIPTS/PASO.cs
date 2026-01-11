28c39a254ffde48a2a847934916ff1b3cb45510e
DerekBaasch <98423502+DerekBaasch@users.noreply.github.com>
2026-01-11 21:41:25 +0100

    ultimos retoques previos al lanzamiento

---
+++ Assets/DerekBuild/SCRIPTS/PASO.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PASO : MonoBehaviour
+{
+
+
+    public void Play()
+    {
+        SceneManager.LoadScene("DÍA 2 - MAÑANA");
+    }
+}
