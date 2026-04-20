24bd8d1ee80c70dbd3511d3cefa202a3db77f456
wuhancho <151606064+wuhancho@users.noreply.github.com>
2026-04-20 14:01:00 +0200

    primera parte notas

---
+++ Assets/Scripts/Personajes/NoteData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using UnityEngine;
+
+public enum NoteType
+{
+    Lie,
+    Truth,
+    Unique
+}
+public class NoteData : MonoBehaviour
+{
+    private int index;
+    private NoteType noteType;
+
+}
--- Assets/Scripts/Personajes/NotesSave.cs
+++ Assets/Scripts/Personajes/NotesSave.cs
@@ -1,54 +1,11 @@
+using System;
+using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
-
-public class NotesSave : MonoBehaviour
+[CreateAssetMenu(fileName = "NotesSave", menuName = "Scriptable Objects/NotesSafes")]
+public class NotesSave : ScriptableObject
 {
-    [SerializeField] private GameObject namePenitent;
-    [SerializeField] private GameObject[] notesLies;
-    [SerializeField] private GameObject[] notesTruths;
-    [SerializeField] private GameObject[] notesUnique;
+    [SerializeField] private List<NoteData> _notesData = new List<NoteData>();
 
-    public GameObject NamePenitent { get => namePenitent; }
-    public GameObject[] NotesLies { get => notesLies; }
-    public GameObject[] NotesTruths { get => notesTruths; }
-    public GameObject[] NotesUnique { get => notesUnique; }
-    public void GetTruthNote(int index, out GameObject note)
-    {
-        foreach (GameObject noteTruth in notesTruths)
-        {
-            string[] parts = noteTruth.name.Split('.');
-            if (parts.Length > 1 && int.TryParse(parts[2], out int noteIndex) && noteIndex == index)
-            {
-                note = noteTruth;
-                return;
-            }
-        }
-        note = null;
-    }
-    public void GetLieNote(int index, out GameObject note)
-    {
-        foreach (GameObject noteLie in notesLies)
-        {
-            string[] parts = noteLie.name.Split('.');
-            if (parts.Length > 1 && int.TryParse(parts[2], out int noteIndex) && noteIndex == index)
-            {
-                note = noteLie;
-                return;
-            }
-        }
-        note = null;
-    }
-    public void GetUniqueNote(int index, out GameObject note)
-    {
-        foreach (GameObject noteUnique in notesUnique)
-        {
-            string[] parts = noteUnique.name.Split('.');
-            if (parts.Length > 1 && int.TryParse(parts[1], out int noteIndex) && noteIndex == index)
-            {
-                note = noteUnique;
-                return;
-            }
-        }
-        note = null;
-    }
 }
