1000ce78d14f14349ebac501199d8482c8ef6e76
DerekBaasch <98423502+DerekBaasch@users.noreply.github.com>
2026-06-01 00:08:17 +0200

    act final y build

---
+++ Assets/Scripts/Menus/FINAL.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FINAL : MonoBehaviour
+{
+  
+
+    public void Play()
+    {
+        SceneManager.LoadScene("MENU FINAL");
+    }
+
+}
